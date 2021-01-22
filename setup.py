7a536b12276f66c77415b4c8a32ea1d5e7147cf3
Andrii Fedorko <andrii.fedorko01@gmail.com>
2021-01-22 19:02:50 +0200

    Add requirements.txt and setup.py. Use `pip install .` to install required packages.

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import find_packages, setup
 setup(
     name='datasciencehackathonanalytical',
-    extras_require=dict(tests=['pytest']),
+    extras_require=dict(tests=['pytest', 'pandas', 'numpy']),
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    install_reqiures=['pytest', 'pandas', 'numpy']
 )

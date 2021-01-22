f02fc61dbad939f51b62ccc7e1aead715485d8d9
Andrii Fedorko <andrii.fedorko01@gmail.com>
2021-01-22 18:03:06 +0200

    Initialize empty PyCharm project. Write trivial failing automated test

---
+++ src/use_case.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+import numpy as np
+# test.csv
+from sklearn.model_selection import GridSearchCV
+
+
+def execute():
+    pass
+
+

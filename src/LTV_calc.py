9f3a7c67e32a4f1acd21995130bae65271420d37
Andrii Fedorko <andrii.fedorko01@gmail.com>
2021-01-24 20:23:43 +0200

    Buletproof LTV works for at least one and two users

--- src/LTV_calc.py
+++ src/LTV_calc.py
@@ -5,28 +5,43 @@ class CustomerLifetimeValue:
 
 
     def __init__(self):
+        pass
+
+    def compute_lifetime_value(self, param):
         # importing data
         self.statTable = None
-        inp = pd.read_csv("model/LTV=USD34.96.csv")
+        inp = pd.read_csv(param)
         inp['Event Date'] = inp['Event Date'].astype('datetime64')
 
         # grouping data by users: finding out amount of their subscriptions and registration dates
         grouped = inp.groupby('Subscriber ID')
+        #print(grouped)
         # aggregating transcations in a table with columns for ID, number of subscriptions for every customer, and registation date, making it easier to later calculate total amounts
-        self.aggregated = grouped.agg({'Event Date': ['count', 'max']})['Event Date'].rename(
-            columns={"max": "registration", "count": "subscriptions"})
-       # print(self.aggregated)
-
-
-    def compute_lifetime_value(self, param):
+        self.aggregated = grouped.agg({'Event Date': ['count', 'min']})['Event Date'].rename(
+            columns={"min": "registration", "count": "subscriptions"})
+        #print(self.aggregated)
         self.__count_how_much_users_having_specific_amount_of_subcriptions()
-        print(self.statTable['subscriptions'])
+
+        # # counting the statistics Table - what amount of users have at least a specific amount(x) of subscriptions
+        self.statTable['registration'] = np.cumsum(self.statTable['registration'])
+        self.statTable = self.statTable.sort_values('subscriptions')
+        # calculating conversion rates
+        # 1.0
+        Client_amounts = self.statTable['registration'] # third column: amount of people who bought exactly x subscriptions(x from 0 to 5)
+        self.Conversion_percents = [1.] + list(np.array(Client_amounts[1:]) / np.array(Client_amounts[:-1]))
+        # # conversion in the meaning of what percentage of those who bought x subscriptions also bought the (x+1) one
+        # convs = np.array(self.Conversion_percents[1:])
         dev_proceeds = 9.99 * 0.7  # including deduction of subscription cost of 30% by Apple
-        ltv = self.statTable['subscriptions']#sum(values[1:])
-        first_client = 0
-        print("\nLTV = ", ltv[first_client])
-        trial = 1
-        return (ltv[first_client] - trial)*dev_proceeds #34.965
+        # # calculating LTV using the formula given in the task
+        sum = 0
+        for i in self.statTable['subscriptions']:
+            sum += (i) -1
+        return sum*dev_proceeds/len(self.statTable)
+       # ltv = statTable['subscriptions']  # sum(values[1:])
+      #  first_client = 0
+      #  print("\nLTV = ", ltv[first_client])
+      #  trial = 1
+       # return (ltv[first_client] - trial) * dev_proceeds  # 34.965
 
     def get_number_of_users(self, parm):
         print(self.aggregated.count())
@@ -37,3 +52,4 @@ class CustomerLifetimeValue:
         self.statTable = self.aggregated.groupby('subscriptions').count().reset_index()
         print(self.statTable)
         self.statTable = self.statTable.sort_values('subscriptions', ascending=False)
+        print(self.statTable['subscriptions'][0] -1)
--- test/test_lifetime_value.py
+++ test/test_lifetime_value.py
@@ -4,15 +4,43 @@ from LTV_calc import *
 
 ltv = CustomerLifetimeValue()
 
-def test_nothing():
-    assert ltv.compute_lifetime_value("../test/LTV=USD34.96.csv") == pytest.approx(34.965, 0.004)
+def test_one_user():
+    assert ltv.compute_lifetime_value("../test/model/LTV=USD34.96.csv") == pytest.approx(34.965, 0.004)
 
 # TODO write more tests with different LTV
 
 
 def test_number_of_users_is_one():
-    assert ltv.get_number_of_users("../test/LTV=USD34.96.csv") == 1
+    assert ltv.get_number_of_users("../test/model/LTV=USD34.96.csv") == 1
 
 
 def test_ltv_when_number_of_users_is_two():
-    assert ltv.compute_lifetime_value("../test/two_users") == pytest.approx(5.544, 0.004)
+    assert ltv.compute_lifetime_value("../test/model/two_users.csv") == pytest.approx(27.971999999999998, 0.004)
+
+
+def test_conversion_rate():
+    ltv.compute_lifetime_value("../test/model/LTV=USD34.96.csv") == pytest.approx(34.965, 0.004)
+    assert ltv.Conversion_percents == [1.0]
+
+    ltv.compute_lifetime_value("../test/model/three_users.csv") == pytest.approx(34.965, 0.004)
+    assert ltv.Conversion_percents == [1.0, pytest.approx(2/3, 0.0001), 0.50]
+
+
+def test_stat_table_one_user():
+    ltv.compute_lifetime_value("../test/model/LTV=USD34.96.csv")
+    print(":-", ltv.statTable)
+    assert ltv.statTable['subscriptions'][0] == 6
+
+# def test_stat_table():
+#     ltv.compute_lifetime_value("../test/model/three_users.csv")
+#     print(":-", ltv.statTable)
+#     print("_", np.array(ltv.statTable['conversion'][:]), "len", len(np.array(ltv.statTable['conversion'][1:])))
+#     assert ltv.statTable == 0
+#
+# def test_stat_table():
+#     ltv.compute_lifetime_value("../test/model/two_users.csv")
+#     print(":-", ltv.statTable)
+#     print("_", np.array(ltv.statTable['conversion'][:]), "len", len(np.array(ltv.statTable['conversion'][1:])))
+#     assert ltv.statTable == 0
+#def test_ltv_when_number_of_users_is_two():
+#    assert ltv.compute_lifetime_value("../test/three_users") == pytest.approx(9*0.99*0.7, 0.004)

5501515c6871dd3d2cd727ecdf517bb05c3d15e9
Andrii Fedorko <andrii.fedorko01@gmail.com>
2021-01-25 19:37:18 +0200

    Write test

--- test/test_present_ltv_across_devices.py
+++ test/test_present_ltv_across_devices.py
@@ -1,5 +1,8 @@
+import os
+
 from presentation import *
 
 
 def test_present_ltv_across_devices():
     present_ltv_compare_between_iphone_and_ipad(15.564, 12.532)
+    os.path.isfile('../OPENME/ltv-iphone-ipad.png')

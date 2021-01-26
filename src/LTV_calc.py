ea05d742c9f32e9070686f571103a30b548d21be
Andrii Fedorko <andrii.fedorko01@gmail.com>
2021-01-26 21:32:23 +0200

    Use automated test to count LTF for three users

--- src/LTV_calc.py
+++ src/LTV_calc.py
@@ -12,56 +12,21 @@ class CustomerLifetimeValue:
         self.aggregated = grouped.agg({'Event Date': ['count', 'max']})['Event Date'].rename(
             columns={"max": "registration", "count": "subscriptions"})
 
-    # conversion in the meaning of what percentage of those who bought x subscriptions also bought the (x+1) one
-    def compute_conversion_percents(self):
-        statTable = self.help_compute_user_retention()
-        # calculating conversion rates
-        Client_amounts = statTable[
-            'registration']  # third column: amount of people who bought exactly x subscriptions(x from 0 to 5)
-        Conversion_percents = [1.] + list(np.array(Client_amounts[1:]) / np.array(Client_amounts[:-1]))
-        return Conversion_percents
+    def compute_ltv_main(self):
+        convs = self.compute_relative_user_conversion_rate_or_raise_error(
+            self.compute_retention())
+        return self.compute_lifetime_value_using_conversions(convs)
 
-    def compute_ltv_from_conversions(self, conversion_rates_without_trial):
-        convs = conversion_rates_without_trial
-        dev_proceeds = 9.99 * 0.7  # including deduction of subscription cost of 30% by Apple
-        # calculating LTV using the formula given in the task
-        values = [dev_proceeds]
-        for _ in range(len(convs)):
-            values.append(values[-1] * convs[_])
-        ltv = sum(values[1:])
-        print("\nLTV = ", ltv)
-        return ltv  # paid_weeks*dev_proceeds/len(self.statTable)
-
-    def _deprecated_compute_lifetime_value(self):
-        return self.compute_ltv_from_conversions(self.compute_conversion_percents()[1:])  # paid_weeks*dev_proceeds/len(self.statTable)
+    def compute_retention(self):
+        user_retention = self.compute_user_retention()
+        return self.extract_active_users(user_retention['subscriptions'], user_retention['registration'])#self.aggregated #[1, 0, 1, 1, 1, 1]
 
 
-    def get_number_of_users(self, parm):
-        print(self.aggregated.count())
-        return len(self.aggregated)
-
-    # As all users in dataset have complete lifetimes, we can count conversions using all records on each step
-    def __count_how_much_users_having_specific_amount_of_subcriptions(self):
-        # statTable = aggregated.groupby('subscriptions').count().reset_index()
-        statTable = self.aggregated.groupby(by="subscriptions").count().reset_index()
-        statTable = statTable.sort_values('subscriptions', ascending=False)
-        return statTable
-
-    def help_compute_user_retention(self):
+    def compute_user_retention(self):
         statTable = self.__count_how_much_users_having_specific_amount_of_subcriptions()
         # counting the statistics Table - what amount of users have at least a specific amount(x) of subscriptions
         statTable['registration'] = np.cumsum(statTable['registration'])
         return statTable.sort_values('subscriptions')
-
-    def compute_retention(self):
-        user_retention = self.help_compute_user_retention()
-        return self.extract_active_users(user_retention['subscriptions'], user_retention['registration'])#self.aggregated #[1, 0, 1, 1, 1, 1]
-
-    def compute_active_users_for_each_week(self):
-        self.transactions['Event Date'] = self.transactions['Event Date'].astype('datetime64')
-        # grouping data by users: finding out amount of their subscriptions and registration dates
-        grouped = self.transactions.groupby('Subscriber ID')
-        return self.aggregated
 
     def extract_active_users(self, using_period_in_weeks, users_for_given_week):
         users = []
@@ -78,22 +43,39 @@ class CustomerLifetimeValue:
         return users
 
     def compute_relative_user_conversion_rate_or_raise_error(self, user_retentions):
-        if len(user_retentions)== 1:
+        if len(user_retentions) == 1:
             return []
-        if (all(user_retentions[i] <= user_retentions[i + 1] for i in range(len(user_retentions) - 1))):
+        if (any(user_retentions[i] < user_retentions[i + 1] for i in range(len(user_retentions) - 1))):
             raise ValueError("Number of users who paid at least ${A money} cannot be greater than number of users who paid ${A money} + ${B money} because one is subset of the other. str(broken_user_retentions): "+str(user_retentions))
+
+        # conversion rate in the meaning of what percentage of those who bought x subscriptions also bought the (x+1) one.
         return list(np.array(user_retentions[1:])/np.array(user_retentions[:-1]))
 
-    def compute_ltv_main(self):
-        user_retention = self.help_compute_user_retention()
-        return self.compute_lifetime_value_using_numbers_of_user_retention(user_retention['subscriptions'], user_retention['registration'])
-
-    def compute_lifetime_value_using_numbers_of_user_retention(self, at_least_subscription, user_retention):
-        convs = self.compute_relative_user_conversion_rate_or_raise_error(self.extract_active_users(at_least_subscription, user_retention))
+    # third column: amount of people who bought exactly x subscriptions(x from 0 to 5)
+    def compute_lifetime_value_using_conversions(self, convs):
         dev_proceeds = 9.99 * 0.7  # including deduction of subscription cost of 30% by Apple
         # calculating LTV using the formula given in the task
         values = [dev_proceeds]
-        for _ in range(len(convs)):
-            values.append(values[-1] * convs[_])
+        for i in range(len(convs)):
+            values.append(values[-1] * convs[i])
         ltv = sum(values[1:])
         return ltv
+
+
+
+
+    def __count_how_much_users_having_specific_amount_of_subcriptions(self):
+        statTable = self.aggregated.groupby(by="subscriptions").count().reset_index()
+        # grouping data by users: finding out amount of their subscriptions and registration dates
+        statTable = statTable.sort_values('subscriptions', ascending=False)
+        return statTable
+
+
+    # As all users in dataset have complete lifetimes, we can count conversions using all records on each step
+
+
+
+
+
+
+
--- test/test_lifetime_value.py
+++ test/test_lifetime_value.py
@@ -3,34 +3,33 @@ import pytest
 from LTV_calc import *
 
 
-@pytest.mark.skip("Fix LTV computation in the code")
 def test_one_user():
-    assert ltv._deprecated_compute_lifetime_value("../test/model/LTV=USD34.96.csv") == pytest.approx(34.965, 0.004)
-
-# TODO write more tests with different LTV
-
-@pytest.mark.skip("Fix LTV computation in the code")
-def test_number_of_users_is_one():
-    assert ltv.get_number_of_users("../test/model/LTV=USD34.96.csv") == 1
+    ltv = CustomerLifetimeValue("../test/model/LTV=USD34.96.csv")
+    assert ltv.compute_relative_user_conversion_rate_or_raise_error([1, 1, 1, 1, 1])
+    assert ltv.compute_ltv_main() == pytest.approx(34.965, 0.004)
 
 
 def test_ltv_when_number_of_users_is_two():
     ltv = CustomerLifetimeValue("../test/model/two_users.csv")
+    assert ltv.compute_relative_user_conversion_rate_or_raise_error([2, 2, 2, 2, 1, 1]) == [1, 1, 1, 0.5, 1]
     assert ltv.compute_ltv_main() == pytest.approx(9.99 * 0.7 * 8 / 2, 0.004)
 
 
-@pytest.mark.skip("Deprecated")
 def test_conversion_rate():
-    ltv._deprecated_compute_lifetime_value("../test/model/LTV=USD34.96.csv")
-    assert ltv.Conversion_percents == [1.0]
+    ltv = CustomerLifetimeValue("../test/model/LTV=USD34.96.csv")
+    assert ltv.compute_relative_user_conversion_rate_or_raise_error(ltv.compute_retention()) == [1.0, 1.0, 1.0, 1.0, 1.0]
+    ltv = CustomerLifetimeValue("../test/model/dummy.csv")
+    assert ltv.compute_relative_user_conversion_rate_or_raise_error([3, 3, 2, 1, 1]) == [1.0, pytest.approx(2 / 3, 0.0001), 0.50, 1]
 
-    ltv._deprecated_compute_lifetime_value("../test/model/three_users.csv")
-    assert ltv.Conversion_percents == [1.0, pytest.approx(2/3, 0.0001), 0.50]
 
-
-def test_three_users():
+def test_ltv_for_three_users():
     ltv = CustomerLifetimeValue("../test/model/three_users.csv")
-    ltv._deprecated_compute_lifetime_value()
+    assert ltv.compute_retention() == [3, 3, 3, 2, 1]
+
+    assert ltv.compute_relative_user_conversion_rate_or_raise_error([3, 3, 3, 2, 1]) == [1, 1, pytest.approx(2 / 3, 0.0001), 0.5]
+    assert ltv.compute_lifetime_value_using_conversions([1, 1, 2 / 3, 0.5]) == (9)*9.99*.7 / 3
+
+
 
 @pytest.mark.skip("Fix LTV computation in the code")
 def test_stat_table_one_user():
@@ -40,16 +39,15 @@ def test_stat_table_one_user():
     print(":-", ltv.statTable)
     assert ltv.statTable['subscriptions'][0] == 6
 
+
 def test_compute_lifetime_value_when_four_users():
     ltv = CustomerLifetimeValue("../test/model/four_users_all_trial.csv")
     assert ltv.compute_ltv_main() == 0
     ltv = CustomerLifetimeValue("../test/model/four_users_one_subscriber.csv")
     assert ltv.compute_ltv_main() == 9.99 * 0.7 * 3 / 4
 
-#def test_compute_lifetime_value_when_four_users():
-#    assert ltv.compute_lifetime_value("../test/model/five_users.csv") == 10 * 0.7 * 9.
 
-#@pytest.mark.skip("To be implemented by Valerii")
+
 def test_compute_relative_user_conversion_rate():
     ltv = CustomerLifetimeValue("model/LTV=USD34.96.csv")
     assert ltv.compute_relative_user_conversion_rate_or_raise_error([8, 4]) == [0.5]
@@ -62,20 +60,16 @@ def test_compute_relative_user_conversion_rate():
 
 def test_functions_talk():
     ltv = CustomerLifetimeValue("model/dummy.csv")
-    at_least_subscription = (2, 4, 6)
     num_of_trial_users = 5
-    user_retention = (num_of_trial_users, 3, 1)
     #trial, week1, w2, w3,w4,w5
     #  [5,   5,    3,  3, 1, 1]
-    assert ltv.compute_lifetime_value_using_numbers_of_user_retention(at_least_subscription, user_retention) == pytest.approx((5+3+3+1+1)*9.99*0.7 / num_of_trial_users, 0.001)
+    assert ltv.compute_lifetime_value_using_conversions([5/5, 3/5, 3/3, 1/3, 1/1]) == pytest.approx((5+3+3+1+1)*9.99*0.7 / num_of_trial_users, 0.001)
 
     at_least_subscription = (2, 4, 6)
-
 
 
 
 def test_integration_whole_system_total_ltv():
     ltv = CustomerLifetimeValue("../src/data_analytics.csv")
-    assert 5 < ltv._deprecated_compute_lifetime_value() < 40
-    print(ltv._deprecated_compute_lifetime_value())
-    #assert ltv.compute_lifetime_value("../src/data_analytics.csv") == -1
+    assert 5 < ltv.compute_ltv_main() < 40
+    assert ltv.compute_ltv_main() == pytest.approx(9.34, 0.001)
